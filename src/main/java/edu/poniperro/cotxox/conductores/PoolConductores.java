063338e7e137b14aa38b3db98acd6267459a87cb
stonarini <stonarini@cifpfbmoll.eu>
2022-02-14 02:05:24 +0100

    feat(PoolConductores): add method to assign a Conductor

--- src/main/java/edu/poniperro/cotxox/conductores/PoolConductores.java
+++ src/main/java/edu/poniperro/cotxox/conductores/PoolConductores.java
@@ -1,7 +1,7 @@
 package edu.poniperro.cotxox.conductores;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class PoolConductores {
 	private List<Conductor> poolConductores;
@@ -12,5 +12,17 @@ public class PoolConductores {
 
 	public List<Conductor> getPoolConductores() {
 		return poolConductores;
+	}
+
+	public Conductor asignarConductor() {
+		Optional<Conductor> conductorAsignado = getPoolConductores().stream().filter(c -> !c.isOcupado()).findAny();
+
+		if (conductorAsignado.isPresent()) {
+			Conductor conductor = conductorAsignado.get();
+			conductor.setOcupado(true);
+			return conductor;
+		} else {
+			return null;
+		}
 	}
 }

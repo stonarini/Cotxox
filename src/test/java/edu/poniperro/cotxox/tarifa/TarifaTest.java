60ae496c22e483e9db6227b3c0480a85b3582de8
stonarini <stonarini@cifpfbmoll.eu>
2022-02-14 02:38:57 +0100

    test: add class imports

--- src/test/java/edu/poniperro/cotxox/tarifa/TarifaTest.java
+++ src/test/java/edu/poniperro/cotxox/tarifa/TarifaTest.java
@@ -4,15 +4,17 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import edu.poniperro.cotxox.carrera.Carrera;
+
 public class TarifaTest {
 	@Test
 	public void costeDistanciaTest() {
-		assertEquals(13.5, Tarifa.getCosteDistancia(10));
+		assertEquals(13.5, Tarifa.getCosteDistancia(10), 0);
 	}
 
 	@Test
 	public void costeTiempoTest() {
-		assertEquals(3.5, Tarifa.getCosteTiempo(10));
+		assertEquals(3.5, Tarifa.getCosteTiempo(10), 0);
 	}
 
 	@Test
@@ -20,7 +22,7 @@ public class TarifaTest {
 		Carrera carrera = new Carrera("ABC12345");
 		carrera.setTiempoEsperado(10);
 		carrera.setDistancia(10);
-		assertEquals(17, Tarifa.getCosteTotalEsperado());
+		assertEquals(17, Tarifa.getCosteTotalEsperado(carrera), 0);
 	}
 
 	@Test
@@ -28,7 +30,7 @@ public class TarifaTest {
 		Carrera carrera = new Carrera("ABC12345");
 		carrera.setTiempoEsperado(1);
 		carrera.setDistancia(1);
-		assertEquals(5, Tarifa.getCosteTotalEsperado());
+		assertEquals(5, Tarifa.getCosteTotalEsperado(carrera), 0);
 	}
 
 }

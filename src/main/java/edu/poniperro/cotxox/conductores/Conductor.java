f3c1a9cda1bc3510a943f8e84e94b76e5cd874fc
stonarini <stonarini@cifpfbmoll.eu>
2022-02-14 01:30:26 +0100

    feat(Conductor): add Conductor class

---
+++ src/main/java/edu/poniperro/cotxox/conductores/Conductor.java
@@ -0,0 +1,65 @@
+package edu.poniperro.cotxox.conductores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conductor {
+	private String nombre;
+	private String modelo;
+	private String matricula;
+	private double valoracionMedia;
+	private boolean ocupado;
+	private List<Byte> valoraciones = new ArrayList<Byte>();
+
+	public Conductor(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public double getValoracion() {
+		return valoracionMedia;
+	}
+
+	public int getNumeroValoraciones() {
+		return valoraciones.size();
+	}
+
+	public void setValoracion(byte valoracion) {
+		this.valoraciones.add(valoracion);
+		calcularValoracionMedia();
+	}
+
+	private void calcularValoracionMedia() {
+		int sumValoraciones = valoraciones.stream()
+				.map(b -> Byte.toUnsignedInt(b))
+				.reduce(0, (a, b) -> a + b);
+		this.valoracionMedia = sumValoraciones / getNumeroValoraciones();
+	}
+
+	public void setOcupado(boolean ocupado) {
+		this.ocupado = ocupado;
+	}
+
+	public boolean isOcupado() {
+		return ocupado;
+	}
+}

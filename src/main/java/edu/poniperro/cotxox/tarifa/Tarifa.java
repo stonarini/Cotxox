f35dce34013a049bb0eba9101b8248813252b3ce
stonarini <stonarini@cifpfbmoll.eu>
2022-02-14 02:19:47 +0100

    feat(Tarifa): add static class Tarifa

---
+++ src/main/java/edu/poniperro/cotxox/tarifa/Tarifa.java
@@ -0,0 +1,22 @@
+package edu.poniperro.cotxox.tarifa;
+
+public class Tarifa {
+
+	final static double COSTE_MILLA = 1.35;
+	final static double COSTE_MINUTO = 0.35;
+	final static double COSTE_MINIMO = 5.0;
+	final static byte PORCENTAJE_COMISION = 20;
+
+	public static double getCosteDistancia(double distance) {
+		return distance * COSTE_MILLA;
+	}
+
+	public static double getCosteTiempo(int minutes) {
+		return minutes * COSTE_MINUTO;
+	}
+
+	public static double getCosteTotalEsperado(Carrera carrera) {
+		double costeTotal = getCosteDistancia(carrera.getDistancia()) + getCosteTiempo(carrera.getTiempoEsperado());
+		return costeTotal > COSTE_MINIMO ? costeTotal : COSTE_MINIMO;
+	}
+}

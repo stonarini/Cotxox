5970efb5436099beeb0a9e4a96eca9b16d9f0bcb
stonarini <stonarini@cifpfbmoll.eu>
2022-02-14 01:49:39 +0100

    feat(PoolConductores): add constructor method

---
+++ src/main/java/edu/poniperro/cotxox/conductores/PoolConductores.java
@@ -0,0 +1,12 @@
+package edu.poniperro.cotxox.conductores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PoolConductores {
+	private List<Conductor> poolConductores;
+
+	public PoolConductores(List<Conductor> conductores) {
+		this.poolConductores = conductores;
+	}
+}
